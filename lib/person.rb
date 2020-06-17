857efeffba18810c1227d357f4b96ead0548a22d
Trevor Cheche <twitcherfrost@gmail.com>
2020-06-17 06:59:39 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+  def name = (person_name)
+    @this_person_name = person_name
+  end
+  def name
+    @this_person_name
+  end
+  def job = (job_name)
+    @this_person_job = job_name
+  end
+  def job
+    @this_person_job
+  end
+end
